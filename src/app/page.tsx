71926432ef6fe628554659685b158fda9a31a820
zl <zl@gmail.com>
2023-02-28 17:25:21 +0800

    Revert "test"
    This reverts commit a5cf8cbaecbcad69b942c6397eb0c4231bada5c8.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-slate-100">
       <h1 className="text-2xl font-bold">Next.js JueJin Blog</h1>
-      <a className="pt-3 text-primary-500 underline" href="/blog">全部文章</a>
+      <Link className="pt-3 text-primary-500 underline" href="/blog">全部文章</Link>
     </main>
   )
 }
